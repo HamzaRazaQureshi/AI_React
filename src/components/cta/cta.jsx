cf9e74e4ea4652de7e003b52590bc06315a7d1e2
unknown <hamzarq999@gmail.com>
2023-03-09 20:40:41 +0500

    Added article, cta and blog section

--- src/components/cta/cta.jsx
+++ src/components/cta/cta.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const cta = () => {
   return (
-    <div>cta</div>
+    <div className="ai__cta">
+      <div className="ai__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="ai__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
